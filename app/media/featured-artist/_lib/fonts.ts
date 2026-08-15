5f305c342828be1c672132d1ae5bec6abe6685b5
FAFONationHQ <islandelegance@gmail.com>
2026-08-15 05:06:38 -0700

    Apply Another Danger font to Velvet Witch page

---
+++ app/media/featured-artist/_lib/fonts.ts
@@ -0,0 +1,3 @@
+import localFont from 'next/font/local';
+
+export const anotherDanger = localFont({ src: '../../../../public/fonts/Another-Danger.otf', display: 'swap', fallback: ['Arial Black', 'Impact', 'sans-serif'] });
