e364c89f9be9e820e5c01ae6dafb661ab61cb082
FAFONationHQ <islandelegance@gmail.com>
2026-08-15 12:28:34 -0700

    Replace Velvet Witch display font with Secret Solver

--- app/media/featured-artist/_lib/fonts.ts
+++ app/media/featured-artist/_lib/fonts.ts
@@ -1,3 +1,3 @@
 import localFont from 'next/font/local';
 
-export const anotherDanger = localFont({ src: '../../../../public/fonts/Another-Danger.otf', display: 'swap', fallback: ['Arial Black', 'Impact', 'sans-serif'] });
+export const secretSolver = localFont({ src: '../../../../public/fonts/Secret Solver.otf', display: 'swap', fallback: ['Arial Black', 'Impact', 'sans-serif'] });
